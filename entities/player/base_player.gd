10bdcdcb4a8c2d7a6e143efc319c835eb714a87a
Alex <oleksandr.kry@gmail.com>
2026-05-29 04:15:19 +0800

    basic physics and movement

--- entities/player/base_player.gd
+++ entities/player/base_player.gd
@@ -1,35 +1,64 @@
 extends CharacterBody2D
 class_name BasePlayer
 
-var grid_pos : Vector2 = Vector2.ZERO 
+# Grid position (logical movement)
+var grid_pos: Vector2 = Vector2.ZERO
 
-#isometric diamond size
-const TILE_WIDTH = 500
-const TILE_HEIGHT = 290
+# Isometric tile size
+const TILE_WIDTH = 128
+const TILE_HEIGHT = 64
+
+# Collision shape used for prediction
+var collision_shape: CircleShape2D
 
 func _ready():
-	#snap to the starting position immediately
+	# Create small collision check shape
+	collision_shape = CircleShape2D.new()
+	collision_shape.radius = 1.0
+
+	# Snap to start position
 	position = grid_to_world(grid_pos)
 
+
 func _process(_delta):
-	#godot arrow key actions (for now)
 	if Input.is_action_just_pressed("ui_right"):
-		move_to_grid(Vector2(1, 0))
+		try_move(Vector2(1, 0))
 	elif Input.is_action_just_pressed("ui_left"):
-		move_to_grid(Vector2(-1, 0))
+		try_move(Vector2(-1, 0))
 	elif Input.is_action_just_pressed("ui_down"):
-		move_to_grid(Vector2(0, 1))
+		try_move(Vector2(0, 1))
 	elif Input.is_action_just_pressed("ui_up"):
-		move_to_grid(Vector2(0, -1))
+		try_move(Vector2(0, -1))
 
-func move_to_grid(direction: Vector2):
-	grid_pos += direction
-	
-	var target_screen_pos = grid_to_world(grid_pos)
-	
-	position = target_screen_pos
 
-#converts cartesian (0,1) to isometric screen coordinates
+# ----------------------------
+# GRID + PHYSICS CHECK
+# ----------------------------
+func try_move(direction: Vector2):
+
+	var new_grid_pos = grid_pos + direction
+	var target_world_pos = grid_to_world(new_grid_pos)
+
+	var query = PhysicsShapeQueryParameters2D.new()
+	query.shape = collision_shape
+	query.transform = Transform2D(0, target_world_pos)
+
+	query.collide_with_bodies = true
+	query.collide_with_areas = true  
+	query.exclude = [self.get_rid()]
+
+	var result = get_world_2d().direct_space_state.intersect_shape(query)
+
+	if result.is_empty():
+		grid_pos = new_grid_pos
+		position = target_world_pos
+	else:
+		print("Blocked by:", result[0].collider.name)
+
+
+# ----------------------------
+# ISOMETRIC CONVERSION
+# ----------------------------
 func grid_to_world(grid_coords: Vector2) -> Vector2:
 	var screen_x = (grid_coords.x - grid_coords.y) * (TILE_WIDTH / 2.0)
 	var screen_y = (grid_coords.x + grid_coords.y) * (TILE_HEIGHT / 2.0)
