b6184edab12557da8fbe61cad18dd57eb9e503a1
Ni Komang Ayu Juliana <124333724+AyuJuliana@users.noreply.github.com>
2026-06-03 17:08:01 +0800

    Update pause menu and MainLevel

--- core/pause_menu.gd
+++ core/pause_menu.gd
@@ -25,7 +25,6 @@ func _on_exit_pressed() -> void:
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://ui/menu.tscn")
 
-
 func _on_pause_button_pressed() -> void:
 	show() 
 	get_tree().paused = true
