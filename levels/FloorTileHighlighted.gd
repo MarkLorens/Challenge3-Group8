36e8c5fb65d2a08098b0ebc0c1bbeff9e9d27c09
MarkLorens <marklorens0705@gmail.com>
2026-06-03 14:24:12 +0800

    Base movement done

--- levels/FloorTileHighlighted.gd
+++ levels/FloorTileHighlighted.gd
@@ -1,16 +1,29 @@
 extends TileMapLayer
 
 @export var floor_tilemap: TileMapLayer
+@export var wall_tilemap: TileMapLayer
 
 func show_move_range(center: Vector2i, move_range: int):
-		clear()
-		for x in range(-move_range, move_range + 1):
-			for y in range(-move_range, move_range + 1):
-				if x == 0 and y == 0:
-					continue
-				var tile = center + Vector2i(x, y)
-				if abs(x) + abs(y) > move_range:
-					continue
-				if floor_tilemap.get_cell_source_id(tile) == -1:
-					continue
-				set_cell(tile, 0, Vector2i.ZERO)
+	clear()
+	
+	var visited: Dictionary = {}
+	var queue: Array = [[center, 0]]  # [tile, steps_used]
+	visited[center] = true
+	
+	while queue.size() > 0:
+		var current = queue.pop_front()
+		var tile: Vector2i = current[0]
+		var steps: int = current[1]
+		
+		if steps > 0:  # Don't highlight the player's own tile
+			if floor_tilemap.get_cell_source_id(tile) != -1:
+				if wall_tilemap.get_cell_source_id(tile) == -1:
+					set_cell(tile, 0, Vector2i.ZERO)
+		
+		if steps >= move_range:
+			continue
+		
+		for neighbor in NeighboringTile.get_isometric_neighbors(tile):
+			if neighbor not in visited:
+				visited[neighbor] = true
+				queue.append([neighbor, steps + 1])
