cc7730633f84c0a008244de2422035c5826ae18b
Ni Komang Ayu Juliana <124333724+AyuJuliana@users.noreply.github.com>
2026-06-03 09:05:35 +0800

    add_pausemenu

---
+++ core/pause_menu.gd
@@ -0,0 +1,31 @@
+extends Control
+
+func _ready():
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	set_anchors_preset(Control.PRESET_FULL_RECT)
+	hide()
+
+#func open_pause():
+	#show()
+	#get_tree().paused = true
+
+func close_pause():
+	hide()
+	get_tree().paused = false
+
+func _on_continue_pressed() -> void:
+	close_pause()
+
+func _on_restart_pressed() -> void:
+	get_tree().paused = false
+	get_tree().reload_current_scene()
+	
+
+func _on_exit_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://ui/menu.tscn")
+
+
+func _on_pause_button_pressed() -> void:
+	show() 
+	get_tree().paused = true
