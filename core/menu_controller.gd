2fc136054c4a6d1ad9e79c6ca006728f8482668e
Cello Farrel Tanojo <109634862+cellofarrel@users.noreply.github.com>
2026-06-10 17:53:45 +0800

    my kisah
    story

--- core/menu_controller.gd
+++ core/menu_controller.gd
@@ -10,4 +10,4 @@ func _on_continue_pressed() -> void:
 func _on_newgame_pressed() -> void:
 	LevelManager.reset()
 	SaveManager.delete_save()
-	get_tree().change_scene_to_file("res://ui/storywhildchair.tscn")
+	get_tree().change_scene_to_file("res://ui/new_gameone.tscn")
